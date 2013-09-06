3ac78d661e2ba14e16c9f276c7eebc94b1cad3e3
K.Kolotyuk <kk@7bits.com>
2013-09-06 11:39:02 +0700

    Unit tests for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   attr_accessible :title, :description, :image_url, :price
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: {minimum: 10, message: 'minimal length is 10'}
   validates :image_url, allow_blank: true, format: {
       with:
           %r{\.(gif|jpg|png)$}i,
