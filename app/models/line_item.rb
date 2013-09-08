5d7347edaca6c06229a423019404b14720aeb124
Konstantin Kolotyuk <konstantin.kolotyuk@gmail.com>
2013-09-08 16:18:00 +0700

    Price column has been added to line_items table

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,9 +1,9 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id
+  attr_accessible :cart_id, :product_id, :price
   belongs_to :product
   belongs_to :cart
 
   def total_price
-    product.price * quantity
+    price * quantity
   end
 end
